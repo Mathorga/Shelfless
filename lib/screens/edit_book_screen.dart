4f898e8465ec6b7b2fe599eed2d3b41a3be4ebcb
Luka Micheletti <lukamicheletti@MacBook-Air-di-Luka.local>
2022-05-26 22:34:00 +0200

    Fixed dialog size on iOS

--- lib/screens/edit_book_screen.dart
+++ lib/screens/edit_book_screen.dart
@@ -78,40 +78,43 @@ class _EditBookScreenState extends State<EditBookScreen> {
                         builder: (BuildContext context) {
                           return AlertDialog(
                             title: const Text("Authors"),
-                            content: ListView(
-                              shrinkWrap: true,
-                              children: [
-                                ...List.generate(
-                                  _authors.length + 1,
-                                  (int index) => index < _authors.length
-                                      ? ListTile(
-                                          leading: Text("${_authors.getAt(index)!.firstName} ${_authors.getAt(index)!.lastName}"),
-                                          onTap: () {
-                                            final Author author = _authors.getAt(index)!;
+                            content: SizedBox(
+                              width: 300.0,
+                              child: ListView(
+                                shrinkWrap: true,
+                                children: [
+                                  ...List.generate(
+                                    _authors.length + 1,
+                                    (int index) => index < _authors.length
+                                        ? ListTile(
+                                            leading: Text("${_authors.getAt(index)!.firstName} ${_authors.getAt(index)!.lastName}"),
+                                            onTap: () {
+                                              final Author author = _authors.getAt(index)!;
 
-                                            // Only add the author if not already there.
-                                            if (!book.authors!.contains(author)) {
-                                              setState(() {
-                                                book.authors?.add(author);
-                                              });
-                                            } else {
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                const SnackBar(
-                                                  content: Text("Author already added"),
-                                                  duration: Duration(seconds: 2),
-                                                ),
-                                              );
-                                            }
-                                            Navigator.of(context).pop();
-                                          },
-                                        )
-                                      : ListTile(
-                                          leading: const Text("Add"),
-                                          trailing: const Icon(Icons.add),
-                                          onTap: () => Navigator.of(context).pushNamed(EditAuthorScreen.routeName),
-                                        ),
-                                ),
-                              ],
+                                              // Only add the author if not already there.
+                                              if (!book.authors!.contains(author)) {
+                                                setState(() {
+                                                  book.authors?.add(author);
+                                                });
+                                              } else {
+                                                ScaffoldMessenger.of(context).showSnackBar(
+                                                  const SnackBar(
+                                                    content: Text("Author already added"),
+                                                    duration: Duration(seconds: 2),
+                                                  ),
+                                                );
+                                              }
+                                              Navigator.of(context).pop();
+                                            },
+                                          )
+                                        : ListTile(
+                                            leading: const Text("Add"),
+                                            trailing: const Icon(Icons.add),
+                                            onTap: () => Navigator.of(context).pushNamed(EditAuthorScreen.routeName),
+                                          ),
+                                  ),
+                                ],
+                              ),
                             ),
                           );
                         },
@@ -184,40 +187,43 @@ class _EditBookScreenState extends State<EditBookScreen> {
                         builder: (BuildContext context) {
                           return AlertDialog(
                             title: const Text("Genres"),
-                            content: ListView(
-                              shrinkWrap: true,
-                              children: [
-                                ...List.generate(
-                                  _genres.length + 1,
-                                  (int index) => index < _genres.length
-                                      ? ListTile(
-                                          leading: Text(_genres.getAt(index)!.name),
-                                          onTap: () {
-                                            final Genre genre = _genres.getAt(index)!;
+                            content: SizedBox(
+                              width: 300.0,
+                              child: ListView(
+                                shrinkWrap: true,
+                                children: [
+                                  ...List.generate(
+                                    _genres.length + 1,
+                                    (int index) => index < _genres.length
+                                        ? ListTile(
+                                            leading: Text(_genres.getAt(index)!.name),
+                                            onTap: () {
+                                              final Genre genre = _genres.getAt(index)!;
 
-                                            // Only add the genre if not already there.
-                                            if (!book.genres!.contains(genre)) {
-                                              setState(() {
-                                                book.genres?.add(genre);
-                                              });
-                                            } else {
-                                              ScaffoldMessenger.of(context).showSnackBar(
-                                                const SnackBar(
-                                                  content: Text("Genre already added"),
-                                                  duration: Duration(seconds: 2),
-                                                ),
-                                              );
-                                            }
-                                            Navigator.of(context).pop();
-                                          },
-                                        )
-                                      : ListTile(
-                                          leading: const Text("Add"),
-                                          trailing: const Icon(Icons.add),
-                                          onTap: () => Navigator.of(context).pushNamed(EditGenreScreen.routeName),
-                                        ),
-                                ),
-                              ],
+                                              // Only add the genre if not already there.
+                                              if (!book.genres!.contains(genre)) {
+                                                setState(() {
+                                                  book.genres?.add(genre);
+                                                });
+                                              } else {
+                                                ScaffoldMessenger.of(context).showSnackBar(
+                                                  const SnackBar(
+                                                    content: Text("Genre already added"),
+                                                    duration: Duration(seconds: 2),
+                                                  ),
+                                                );
+                                              }
+                                              Navigator.of(context).pop();
+                                            },
+                                          )
+                                        : ListTile(
+                                            leading: const Text("Add"),
+                                            trailing: const Icon(Icons.add),
+                                            onTap: () => Navigator.of(context).pushNamed(EditGenreScreen.routeName),
+                                          ),
+                                  ),
+                                ],
+                              ),
                             ),
                           );
                         },
