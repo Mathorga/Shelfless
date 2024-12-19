5b2d1dc5c44754d104513d26a1d17fccf382792e
Luka Micheletti <michelettiluka@gmail.com>
2024-12-19 17:13:38 +0100

    Started real DB

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:sqflite/sqflite.dart';
+
 class DatabaseHelper {
   // Private instance.
   static final DatabaseHelper _instance = DatabaseHelper._private();
@@ -8,5 +12,120 @@ class DatabaseHelper {
   // Public instance getter.
   static DatabaseHelper get instance => _instance;
 
-  // late Database _db;
+  late Database _db;
+
+  Future<void> openDB() async {
+    // Avoid errors caused by flutter upgrade.
+    WidgetsFlutterBinding.ensureInitialized();
+    // Open the database and store the reference.
+    _db = await openDatabase(
+      "${await getDatabasesPath()}/smart_field_database.db",
+      version: 1,
+      onCreate: (Database db, int version) async {
+        // Perform upgrades from version 1 to the current version.
+        for (int v = 1; v <= version; v++) {
+          upgradeDb(db, v);
+        }
+      },
+      onUpgrade: (Database db, int oldVersion, int newVersion) {
+        // Perform upgrades from oldVersion to newVersion.
+        for (int v = oldVersion + 1; v <= newVersion; v++) {
+          upgradeDb(db, v);
+        }
+      },
+      onDowngrade: (Database db, int oldVersion, int newVersion) {
+        // TODO Handle version downgrade.
+      },
+    );
+  }
+
+  /// Redirects to the correct version upgrade to perform.
+  static Future<void> upgradeDb(Database db, int targetVersion) async {
+    switch (targetVersion) {
+      case 1:
+        await upgradeVersion1(db);
+        break;
+      default:
+        break;
+    }
+  }
+
+  /// Performs database definition for version 1.
+  static Future<void> upgradeVersion1(Database db) async {
+    // Books table.
+    await db.execute(
+      """
+      CREATE TABLE books(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        publishDate INTEGER NOT NULL,
+        publisher INTEGER,
+        location INTEGER,
+        borrowed BOOL,
+        edition INTEGER DEFAULT 0
+      )
+      """,
+    );
+
+    // Authors table.
+    await db.execute(
+      """
+      CREATE TABLE authors(
+        id INTEGER PRIMARY KEY AUTOINCREMENT
+      )
+      """,
+    );
+
+    // Genres table.
+    await db.execute(
+      """
+      CREATE TABLE genres(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        color INTEGER DEFAULT 0xFF000000
+      )
+      """,
+    );
+
+    // Publishers table.
+    await db.execute(
+      """
+      CREATE TABLE publishers(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        website TEXT
+      )
+      """,
+    );
+
+    // Locations table.
+    await db.execute(
+      """
+      CREATE TABLE locations(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+      )
+      """,
+    );
+
+    // Books to authors relationship table.
+    await db.execute(
+      """
+      CREATE TABLE book_author_rel(
+        book_id INTEGER PRIMARY KEY,
+        author_id INTEGER PRIMARY KEY
+      )
+      """
+    );
+
+    // Books to genres relationship table.
+    await db.execute(
+      """
+      CREATE TABLE book_genre_rel(
+        book_id INTEGER PRIMARY KEY,
+        genre_id INTEGER PRIMARY KEY
+      )
+      """
+    );
+  }
 }
