2297b3378cc17378ed655db0cfd4e79bad4132aa
Luka Micheletti <michelettiluka@gmail.com>
2025-02-09 19:24:20 +0100

    Edited new test shaders

--- shaders/test.frag.glsl
+++ shaders/test.frag.glsl
@@ -10,8 +10,8 @@ uniform float THRESHOLD = 0.001;
 uniform float AA_SCALE = 11.0;
 
 bool eq(vec4 col1, vec4 col2) {
-    vec4 dif = col1 - col2;
-    float length_squared = dif.r * dif.r + dif.g * dif.g + dif.b * dif.b + dif.a * dif.a;
+    vec4 diff = col1 - col2;
+    float length_squared = diff.r * diff.r + diff.g * diff.g + diff.b * diff.b + diff.a * diff.a;
     return length_squared < THRESHOLD * THRESHOLD;
 }
 
@@ -19,7 +19,7 @@ bool eq(vec4 col1, vec4 col2) {
 vec4 diag(vec4 sum, vec2 uv, vec2 point1, vec2 point2, vec4 color1, vec4 color2, float LINE_THICKNESS) {
     if (eq(color1, color2)) {
         vec2 dir = point2 - point1;
-        vec2 lp = uv - (floor(uv + point1) + .5);
+        vec2 lp = uv - (floor(uv + point1) + 0.5);
         dir = normalize(vec2(dir.y, -dir.x));
         float l = clamp((LINE_THICKNESS - dot(lp, dir)) * AA_SCALE, 0.0, 1.0);
         sum = mix(sum, color1, l);
@@ -29,8 +29,9 @@ vec4 diag(vec4 sum, vec2 uv, vec2 point1, vec2 point2, vec4 color1, vec4 color2,
 
 void main() {
     float LINE_THICKNESS = 0.4;
-    vec2 ip = UV;
-    ip = UV * (1.0 / TEXTURE_PIXEL_SIZE);
+    vec2 uv = FlutterFragCoord().xy;
+    vec2 ip = uv;
+    // ip = uv * (1.0 / TEXTURE_PIXEL_SIZE);
 
     /*
         lu	u	ru
@@ -38,16 +39,16 @@ void main() {
         ld	d	rd
     */
 
-    vec4 c = texelFetch(image, ivec2(ip), ML);
-    vec4 l = texelFetch(image, ivec2(ip + vec2(-1, 0)), ML);
-    vec4 d = texelFetch(image, ivec2(ip + vec2(0, 1)), ML);
-    vec4 r = texelFetch(image, ivec2(ip + vec2(1, 0)), ML);
-    vec4 u = texelFetch(image, ivec2(ip + vec2(0, -1)), ML);
+    vec4 c = texture(image, ip);
+    vec4 l = texture(image, vec2(ip + (vec2(-1.0, 0.0) / uSize)));
+    vec4 d = texture(image, vec2(ip + (vec2(0.0, 1.0) / uSize)));
+    vec4 r = texture(image, vec2(ip + (vec2(1.0, 0.0) / uSize)));
+    vec4 u = texture(image, vec2(ip + (vec2(0.0, -1.0) / uSize)));
 
-    vec4 ld = texelFetch(image, ivec2(ip + vec2(-1, 1)), ML);
-    vec4 lu = texelFetch(image, ivec2(ip + vec2(-1, -1)), ML);
-    vec4 rd = texelFetch(image, ivec2(ip + vec2(1, 1)), ML);
-    vec4 ru = texelFetch(image, ivec2(ip + vec2(1, -1)), ML);
+    vec4 ld = texture(image, vec2(ip + (vec2(-1.0, 1.0) / uSize)));
+    vec4 lu = texture(image, vec2(ip + (vec2(-1.0, -1.0) / uSize)));
+    vec4 rd = texture(image, vec2(ip + (vec2(1.0, 1.0) / uSize)));
+    vec4 ru = texture(image, vec2(ip + (vec2(1.0, -1.0) / uSize)));
 
     vec4 s = c;
 
@@ -77,5 +78,5 @@ void main() {
     f = diag(f, ip, vec2(0, 1), vec2(1, 0), r, d, LINE_THICKNESS);
     f = diag(f, ip, vec2(1, 0), vec2(0, -1), r, u, LINE_THICKNESS);
 
-    COLOR = (s + f) / 2.0;
+    fragColor = (s + f) / 2.0;
 }
