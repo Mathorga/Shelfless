7495ff942eb03460b9f506c469c96befb963357f
Luka <micheletti@MacBook-Air-di-Luka.local>
2025-11-03 00:27:36 +0100

    Updated sort order menu

--- lib/widgets/library_sort_order_list_widget.dart
+++ lib/widgets/library_sort_order_list_widget.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
 import 'package:shelfless/providers/library_content_provider.dart';
@@ -44,29 +45,40 @@ class _LibrarySortOrderListWidgetState extends State<LibrarySortOrderListWidget>
             strings.librarySortBy,
             style: theme.textTheme.headlineSmall,
           ),
-          SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: Themes.spacingMedium,
-              children: SortOrder.values
-                  .where((SortOrder sortOrder) => sortOrder.enabled)
-                  .map(
-                    (SortOrder sortOrder) => Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          sortOrder.label(),
+          RadioGroup<int>(
+            groupValue: LibraryContentProvider.instance.sortOrder.index,
+            onChanged: (int? value) {
+              // Just return if null is received.
+              if (value == null) return;
+
+              // Set the new value otherwise.
+              LibraryContentProvider.instance.sortBooksBy(SortOrder.values[value]);
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: SortOrder.values
+                    .where((SortOrder sortOrder) => sortOrder.enabled)
+                    .mapIndexed(
+                      (int index, SortOrder sortOrder) => GestureDetector(
+                        onTap: () {
+                          // Set the new value.
+                          LibraryContentProvider.instance.sortBooksBy(sortOrder);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Text(sortOrder.label()),
+                            ),
+                            Radio<int>(value: index),
+                          ],
                         ),
-                        Radio(
-                          value: LibraryContentProvider.instance.sortOrder == sortOrder,
-                          groupValue: true,
-                          onChanged: (bool? value) => LibraryContentProvider.instance.sortBooksBy(sortOrder),
-                        ),
-                      ],
-                    ),
-                  )
-                  .toList(),
+                      ),
+                    )
+                    .toList(),
+              ),
             ),
           ),
         ],
